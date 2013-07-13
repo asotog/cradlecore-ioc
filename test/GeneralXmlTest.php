3cd82e3d3a589a5fb72c16d71e23365c010f4975
asotog88 <asotog88@yahoo.es>
2013-07-13 14:34:50 -0600

    changes on unit tests for resource object attributte test

--- test/GeneralXmlTest.php
+++ test/GeneralXmlTest.php
@@ -20,6 +20,7 @@ include_once('PHPUnit/Autoload.php');
 class GeneralXmlTest extends PHPUnit_Framework_TestCase  {
     protected $context;
     protected $context2;
+    protected $context3;
 
     /**
      * Set Up the context for each test
@@ -32,8 +33,10 @@ class GeneralXmlTest extends PHPUnit_Framework_TestCase  {
         $callerDirectory = dirname(__FILE__);
         $configuration = $callerDirectory . '/helpers/configuration.xml';
         $configuration2 = $callerDirectory . '/helpers/oneObjectConfiguration.xml';
+        $configuration3 = $callerDirectory . '/helpers/configuration2.xml';
         $this->context = Context_Factory::getXmlContext($configuration, true);
         $this->context2 = Context_Factory::getXmlContext($configuration2, true);
+        $this->context3 = Context_Factory::getXmlContext($configuration3, true);
     }
 
     /**
@@ -94,6 +97,15 @@ class GeneralXmlTest extends PHPUnit_Framework_TestCase  {
     function testOneObjectXmlConfiguration(){
         $connection = $this->context2->getObject('connection');
         $this->assertNotNull($connection->getConnectionString());
+    }
+    
+    /**
+     * 
+     * 
+     */
+    function testResourceAttributteOnObject() {
+        $validator = $this->context3->getObject('validator');
+        $this->assertNotNull($validator->isEmpty('hello'));
     }
 }
 
